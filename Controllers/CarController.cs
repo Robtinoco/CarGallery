9cc757ed4e26b0393181cd1e5eeeec762786360f
Robertinho <Robertinho@ROBERTINHO-PC>
2022-11-05 12:43:45 -0300

    Add project files.

---
+++ Controllers/CarController.cs
@@ -0,0 +1,39 @@
+﻿using CarGallery.Models;
+using CarGallery.Repository;
+using CarGallery.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CarGallery.Controllers
+{
+    public class CarController : Controller
+    {
+        public CarGalleryDbContext Context { get; set; }
+
+        public CarController(CarGalleryDbContext context)
+        {
+            this.Context = context;
+        }
+       public IActionResult List(string? brand)
+        {
+            List<Car> cars;
+            
+            if (string.IsNullOrEmpty(brand))
+            {
+                cars = this.Context.Cars.ToList();
+                brand = "Todos os carros";
+            }
+            else
+            {
+                cars = this.Context.Cars.Where(c => c.Brand.Name == brand).ToList();
+            }
+
+            return View(new CarListViewModel()
+            {
+                BrandName = brand,
+                Cars = cars
+            });
+;        }
+    }
+}
---
+++ Models/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace CarGallery.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public string ImageUrl { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public Brand Brand { get; set; }
+        public int BrandId { get; set; }
+
+    }
+}
